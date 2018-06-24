f739952e87755098b86389798bd243167d47ba48
Vinny DeStefano <vjdestefano500@gmail.com>
2018-06-24 17:34:15 -0400

    the test

--- oglite/src/componentsVin/vinJs.js
+++ oglite/src/componentsVin/vinJs.js
@@ -1,19 +1,55 @@
 
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class mainPage extends Component {
-  
-  
-  
-  displayMainPage = ({ articles }) => (
-  <div
-    style={{ height: 300, marginBottom: 30}}
-    className="col-md-8"
-  >
+// import API from "../../../utilities/API";
 
-  //loop through each article and post it to the main page section
-    {articles}
-  </div>
-);
-};
-export default mainPage;
+// class mainPage extends Component {
+  
+  
+//   componentDidMount() {
+//     this.loadArticles();
+//   }
+ 
+//   loadArticles = () => {
+//     API.getArticles()
+//       .then(res =>
+//         this.setState({ articles: res.data, title: "", author: "", synopsis: "" })
+//       )
+//       .catch(err => console.log(err));
+//   };
+
+
+//   render(){
+//     {this.state.articles.length (
+//       <List>
+
+
+//         {this.state.articles.map(article => {
+//           return (
+
+
+//             <ListItem key={article.contentId}>
+//               <a href={"/books/" + article.contentId}>
+//                 <strong>
+//                   {article.contentTitle} by {article.author}
+//                 </strong>
+//               </a>
+//               <DeleteBtn onClick={() => this.deleteBook(article.contentId)} />
+            
+//             {/* change the list item here too */}
+//             </ListItem>
+//           );
+//         })}
+
+//         {/* make a class that changes this from a list to indiviual section  */}
+//       </List>
+//       ) 
+//     }
+//   }
+
+  
+
+
+
+// };
+// export default mainPage;
--- oglite/src/index.js
+++ oglite/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import mainPage from './componentsVin/vinJs'
+// import mainPage from './componentsVin/vinJs'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
